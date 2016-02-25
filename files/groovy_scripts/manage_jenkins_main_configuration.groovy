cd2823135dd09c6187095e1e27317382fa81aa76
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-02-25 21:59:28 +0100

    Initialize main configuration management with groovy, number of executors

---
+++ files/groovy_scripts/manage_jenkins_main_configuration.groovy
@@ -0,0 +1,56 @@
+#!/usr/bin/env groovy
+
+import jenkins.model.*
+import groovy.json.*
+
+
+/* FUNCTIONS */
+def parse_data(String arg) {
+
+    try {
+        def jsonSlurper = new JsonSlurper()
+        return jsonSlurper.parseText(arg)
+    }
+    catch(e) {
+        throw new Exception("Parse data error, incoming data : ${arg}, "
+                            + "error message : ${e.getMessage()}")
+    }
+}
+
+
+def set_number_of_executors(jenkins_instance, executors_number) {
+
+    // Get current value, used to check if changed
+    def cur_value = jenkins_instance.getNumExecutors()
+    if (cur_value == executors_number) {
+        return false
+    }
+
+    try {
+        jenkins_instance.setNumExecutors(executors_number)
+    }
+    catch(Exception e) {
+        throw new Exception('An error occurs during number of executor change')
+    }
+
+    return true
+}
+
+
+/* SCRIPT */
+def changed = false
+def data = [:]
+
+try {
+    def jenkins_instance = Jenkins.getInstance()
+    data = parse_data(args[0])
+
+    // Manage configuration with user data
+    set_number_of_executors(jenkins_instance, data['number_of_executors'])
+}
+catch(Exception e) {
+    throw new RuntimeException(e.getMessage())
+}
+
+println JsonOutput.toJson(changed=changed, output=data.toString())
+
---
+++ library/manage_jenkins_main_configuration.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+
+from ansible.module_utils.basic import *  # NOQA
+import json
+from os.path import basename
+
+
+module_args = dict(
+    number_of_executors=dict(
+        type='int',
+        required=True),
+
+    cli_path=dict(
+        type='str',
+        required=False,
+        default='/var/lib/jenkins/jenkins-cli.jar'),
+    groovy_scripts_path=dict(
+        type='str',
+        required=False,
+        default='/var/lib/jenkins/groovy_scripts'),
+    url=dict(
+        type='str',
+        required=False,
+        default='http://localhost:8080')
+)
+
+
+def main():
+
+    module = AnsibleModule(module_args)
+
+    script = "%s/%s.groovy" % (module.params['groovy_scripts_path'],
+                               basename(__file__))
+
+    rc, stdout, stderr = module.run_command(
+        "java -jar %s -s '%s' groovy %s '%s'" %
+        (module.params['cli_path'], module.params['url'], script,
+            json.dumps(module.params)))
+
+    if (rc != 0):
+        module.fail_json(msg=stderr)
+
+    json_stdout = json.loads(stdout)
+    module.exit_json(changed=bool(json_stdout), output=json_stdout)
+
+
+if __name__ == '__main__':
+    main()
